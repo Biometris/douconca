a8914ca31bb675d96dc13a620df5c533659a3b25
Bart-Jan van Rossum <bart-jan.vanrossum@wur.nl>
2024-05-29 11:35:33 +0200

    Slight restructure of DESCRIPTION and related files

--- R/douconca-package.R
+++ R/douconca-package.R
@@ -1,11 +1,10 @@
-#' Double constrained correspondence analysis for multi-trait multi-environment analysis
+#' Double constrained correspondence analysis for multi-trait multi-environment 
+#' analysis
+#' 
 #' @aliases douconca-package
 #' @name douconca-package
 #' @keywords internal
+#' 
+#' @importFrom stats as.formula
 "_PACKAGE"
-
-## usethis namespace: start
-## @importFrom vegan scores
-## usethis namespace: end
-
 NULL
